5433d23d417e72a591df20adf0ac7f9e586179a8
Jon Magnuson <jon.magnuson@gmail.com>
2019-02-09 15:37:30 -0600

    tests: pass SPN key in by `&str`

--- tests/pgnlibrary.rs
+++ tests/pgnlibrary.rs
@@ -22,7 +22,7 @@ fn pgnlib_build_parse() {
 
     // Pull signal definition for engine speed
     let enginespeed_def: &SpnDefinition = lib
-        .get_spn(&"Engine_Speed").unwrap();
+        .get_spn("Engine_Speed").unwrap();
 
     // Parse frame containing engine speed
     let msg: [u8; 8] = [0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88];
